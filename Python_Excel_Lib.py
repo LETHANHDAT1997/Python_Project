00c0a55ad867ac237f3407cc1d601bfc30a9ab9b
LETHANHDAT1997 <jackbrunor@gmail.com>
2024-06-14 09:58:47 +0700

    update format feature

--- Python_Excel_Lib.py
+++ Python_Excel_Lib.py
@@ -99,26 +99,6 @@ class Excel_WorkBook():
         # Kiểm tra xem tên sheet đã tồn tại chưa
         if self.__check_name_sheet__(str_name_sheet) == True:
             self.Ob_workbook[str_name_sheet][str_cell_index.strip()] = str_content
-            self.Ob_workbook[str_name_sheet][str_cell_index.strip()].fill    = PatternFill(start_color="FFC7CE", end_color="FFC7CE", fill_type = "solid")
-            self.Ob_workbook[str_name_sheet][str_cell_index.strip()].font    = Font(name='Tahoma',size=11,bold=True,italic=False,vertAlign=None,underline='none',strike=False,color='FF000000')
-            self.Ob_workbook[str_name_sheet][str_cell_index.strip()].border  = Border(  left=Side(border_style="double",
-                                                                                                color='FF000000'),
-                                                                                        right=Side(border_style="double",
-                                                                                                color='FF000000'),
-                                                                                        top=Side(border_style="double",
-                                                                                                color='FF000000'),
-                                                                                        bottom=Side(border_style="double",
-                                                                                                    color='FF000000'),
-                                                                                        diagonal=Side(border_style="double",
-                                                                                                    color='FF000000'),
-                                                                                        diagonal_direction=0,
-                                                                                        outline=Side(border_style="double",
-                                                                                                    color='FF000000'),
-                                                                                        vertical=Side(border_style="double",
-                                                                                                    color='FF000000'),
-                                                                                        horizontal=Side(border_style="double",
-                                                                                                    color='FF000000')
-                                                                                    )
         else:
             print(f"Sheet với tên '{str_name_sheet}' không tồn tại.")
 
@@ -152,7 +132,21 @@ class Excel_WorkBook():
         # self.Ob_workbook.active.auto_filter.add_filter_column(0)
         # self.Ob_workbook.active.auto_filter.add_sort_condition(str_column)
         # print(self.Ob_workbook[str_name_sheet].max_row)
-        
+
+
+    def Format_Cells(self,str_name_sheet,str_cell_index,PatternFill,Font,Border):
+        # Kiểm tra xem tên sheet đã tồn tại chưa
+        if self.__check_name_sheet__(str_name_sheet) == True:
+            self.Ob_workbook[str_name_sheet][str_cell_index.strip()].fill    = PatternFill
+            self.Ob_workbook[str_name_sheet][str_cell_index.strip()].font    = Font
+            self.Ob_workbook[str_name_sheet][str_cell_index.strip()].border  = Border
+        else:
+            print(f"Sheet với tên '{str_name_sheet}' không tồn tại.")   
+
+
+    def Merge_Cells(self,str_name_sheet,str_cell_index):
+        self.Ob_workbook.active.merge_cells(str_cell_index) 
+
     def Save(self,path_save):
         return self.Ob_workbook.save(path_save)
     
@@ -165,10 +159,32 @@ if __name__ == "__main__":
     excel_file_path = "example.xlsx"  # Đường dẫn tới file Excel
     excel_sheet_name = "Sheet"       # Tên của sheet trong file Excel
 
+    PatternFill_Base    = PatternFill(start_color="FFC7CE", end_color="FFC7CE", fill_type = "solid")
+    Font_Base           = Font(name='Tahoma',size=11,bold=True,italic=False,vertAlign=None,underline='none',strike=False,color='FF000000')
+    Border_Base         = Border(  left=Side(border_style="double",
+                                    color='FF000000'),
+                                    right=Side(border_style="double",
+                                    color='FF000000'),
+                                    top=Side(border_style="double",
+                                    color='FF000000'),
+                                    bottom=Side(border_style="double",
+                                    color='FF000000'),
+                                    diagonal=Side(border_style="double",
+                                    color='FF000000'),
+                                    diagonal_direction=0,
+                                    outline=Side(border_style="double",
+                                    color='FF000000'),
+                                    vertical=Side(border_style="double",
+                                    color='FF000000'),
+                                    horizontal=Side(border_style="double",
+                                    color='FF000000')
+                                )
+    
     File_example = Excel_WorkBook(excel_file_path,excel_sheet_name)
     File_example.Write_strCell(excel_sheet_name,"B7",25000)
     File_example.Write_strCell(excel_sheet_name,"C7",23000)
     File_example.Add_Sort_Filter(excel_sheet_name,"A1:D7")
-
+    File_example.Merge_Cells(excel_sheet_name,'B2:D4')
+    File_example.Format_Cells(excel_sheet_name,'B7',PatternFill_Base,Font_Base,Border_Base)
     # Phải có lệnh Save thì nội dung thay đổi mới được lưu lại trên bộ nhớ
     File_example.Save(excel_file_path)
